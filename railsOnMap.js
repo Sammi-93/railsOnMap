60f66a771cfdcc159f613f34fcc9a3319bbed7b0
Ajay Singh <aksingh1493@gmail.com>
2017-10-15 15:31:19 +0530

    maps failure

--- railsOnMap.js
+++ railsOnMap.js
@@ -4,7 +4,7 @@ function initMap() {
 	//	zoom: 8,
 	//	center: {lat:40.5937, lng:90.9629}
 	//}
-	let options = {
+	var options = {
 		center : {
 			lat : 37,
 			lng : -122
@@ -66,6 +66,53 @@ function initMap() {
 		.catch(function(error){
 			console.log(error);
 		});
-	}*/
+	}
+	//Geolocation
+	var map, infoWindow;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 6
+        });
+        infoWindow = new google.maps.InfoWindow;
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
+	*/
+	// fetch('http://api.railwayapi.com/v2/live/train/14005/date/15-10-2017/apikey/58d7ikf67k/')
+ //    .then(response => response.json())
+ //    .then(data => {
+ //        // Do what you want with your data
+ //        console.log(data);
+ //    });
+ var searchBox = new google.maps.places.SearchBox(document.getElementById('train-search'));
+	
+
 	
 }
